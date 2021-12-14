90cd5fc731f16150e22fb669b0df62b9e2f100ba
r-mlri <rafael.mallari.cics@ust.edu.ph>
2021-12-15 05:18:01 +0800

    E

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import NavBar from './sitecomp/NavBar';
-import Home from './sitecomp/Home';
-import Router from './sitecomp/Router';
+import Home from './sitecomp/Home.js';
+import Router from './sitecomp/Router.js';
 
 function App() {
   return (
   <div>
     <NavBar/>
     <Router/>
-    <Home/>
   </div>
 
   );
