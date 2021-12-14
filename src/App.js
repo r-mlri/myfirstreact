64dfa62b83eea8d6e60cc673a45a25843a689724
r-mlri <rafael.mallari.cics@ust.edu.ph>
2021-12-15 05:30:34 +0800

    E

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import logo from './pekodead.png';
 import './App.css';
-import NavBar from './sitecomp/NavBar';
-import Home from './sitecomp/Home.js';
-import Router from './sitecomp/Router.js';
+import Button from './sitecomp/Button';
+//import Employee from './sitecomp/Employee';
 
-function App() {
+function App(){
   return (
-  <div>
-    <NavBar/>
-    <Router/>
-  </div>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+                <Button/>
+        <p>
+          Hey you, you're finally awake, you tried to cross the border but ultimately failed didn't you?.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CLICK THIS LINK TO FIND OUT HOW NINE OUT OF TEN EXPERTS HATE THIS MAN WITH HIS ONE SIMPLE TRICK IN LIFE
+        </a>
+      </header>
+    </div>
   );
 }
 
